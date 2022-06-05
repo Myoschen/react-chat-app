ed9630e95303c6a7a9570a18892ecc207c5456d2
Myos <68583272+willy14620@users.noreply.github.com>
2022-06-05 19:31:14 +0800

    refactor: Refactor user join event
    User must leave the previous server berfore joining the next server.

--- server/index.js
+++ server/index.js
@@ -15,16 +15,24 @@ const io = new Server(server, {
   }
 });
 
+let lastServer = '';
+
 io.on('connection', (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  socket.on('join_room', (data) => {
+  socket.on('join', (data) => {
+    if (lastServer !== '') {
+      socket.leave(lastServer);
+      console.log(`User with ID: ${socket.id} left server: ${data}`);
+    } else {
+      lastServer = data;
+    }
     socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log(`User with ID: ${socket.id} joined server: ${data}`);
   });
 
   socket.on('send_message', (data) => {
-    socket.to(data.room).emit('receive_message', data);
+    socket.to(data.server).emit('receive_message', data);
   });
 
   socket.on('disconnect', () => {
