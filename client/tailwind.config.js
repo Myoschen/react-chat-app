6c58e6f33d444069c0885f3041c5973d77c3823e
Myos <68583272+willy14620@users.noreply.github.com>
2022-06-12 23:41:01 +0800

    feat: Add colors and fonts
    Roboto font,  Noto Sans TC

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,8 +4,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        primary: ['Roboto', 'Noto Sans TC', 'sans-serif'],
+      },
       colors: {
-        'custom-dark': '#282a36',
+        primary: {
+          100: "#d4d4d7",
+          200: "#a9aaaf",
+          300: "#7e7f86",
+          400: "#53555e",
+          500: "#282a36",
+          600: "#20222b",
+          700: "#181920",
+          800: "#101116",
+          900: "#08080b"
+        },
       }
     },
   },
